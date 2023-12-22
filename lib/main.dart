00d08c224170b39c105f09c11fe07ec5c042827a
Maksim11011 <mvbspb@bk.ru>
2023-12-23 00:16:05 +0300

    add assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
+        theme: ThemeData(
+          fontFamily: 'Raleway',
+        ),
+        home: const Scaffold(
           body: SafeArea(child: NoiceMeter()),
         )),
   );
@@ -19,19 +22,29 @@ class NoiceMeter extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Padding(
-          padding: const EdgeInsets.all(25),
+          padding: const EdgeInsets.all(15),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: 30,
-                height: 30,
-                color: Colors.red,
+              InkWell(
+                borderRadius: BorderRadius.circular(50),
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.info,
+                  ),
+                ),
               ),
-              Container(
-                width: 30,
-                height: 30,
-                color: Colors.green,
+              InkWell(
+                borderRadius: BorderRadius.circular(50),
+                onTap: () {},
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.settings,
+                  ),
+                ),
               ),
             ],
           ),
@@ -43,39 +56,99 @@ class NoiceMeter extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                width: 400,
+                width: MediaQuery.of(context).size.width,
                 height: 150,
                 color: Colors.blue,
               ),
-              Container(
-                width: 400,
-                height: 50,
-                color: Colors.black54,
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '43.1',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 80,
+                      color: Color.fromARGB(255, 60, 60, 60),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Min 35.1',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.green,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Max76.3',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ],
           ),
         ),
         Row(
           children: [
-            Container(
-              width: 393 / 4,
-              height: 50,
-              color: Colors.green,
+            InkWell(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(18),
+              ),
+              onTap: () {},
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 4,
+                height: 75,
+                child: const Icon(
+                  Icons.save,
+                ),
+              ),
             ),
-            Container(
-              width: 393 / 4,
-              height: 50,
-              color: Colors.lightGreen,
+            InkWell(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(18),
+              ),
+              onTap: () {},
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 4,
+                height: 75,
+                child: const Icon(
+                  Icons.mic,
+                ),
+              ),
             ),
-            Container(
-              width: 393 / 4,
-              height: 50,
-              color: Colors.green,
+            InkWell(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(18),
+              ),
+              onTap: () {},
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 4,
+                height: 75,
+                child: const Icon(
+                  Icons.list,
+                ),
+              ),
             ),
-            Container(
-              width: 393 / 4,
-              height: 50,
-              color: Colors.lightGreen,
+            InkWell(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(18),
+              ),
+              onTap: () {},
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 4,
+                height: 75,
+                child: const Icon(
+                  Icons.timer,
+                ),
+              ),
             ),
           ],
         )
